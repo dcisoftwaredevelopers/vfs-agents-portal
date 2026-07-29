4ad3ffcbcfbc356a41e41a992a220015e9876dbc
Swetha <swethabala2808@gmail.com>
2026-07-29 11:21:42 +0530

    remove comment from admindashboard

--- frontend/src/pages/AdminDashboard.jsx
+++ frontend/src/pages/AdminDashboard.jsx
@@ -52,7 +52,6 @@ function InfoTooltip({ text }) {
   );
 }
 
-// adminin dashboard page
 export default function AdminDashboard() {
   const navigate = useNavigate();
   const user = JSON.parse(localStorage.getItem('adminInfo') || localStorage.getItem('userInfo'));
